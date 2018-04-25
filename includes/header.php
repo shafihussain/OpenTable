123a07f490869174643911948e2481e4fcd69a8a
shafi hussain <shafi.globalempire@gmail.com>
2018-04-24 23:24:28 -0400

    Initial Commit

---
+++ includes/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Open Table</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/mystyle.css" rel="stylesheet">
+
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">OpenTable</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a id="links" class="nav-link" href="#">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Reservation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="includes/register.php">Member Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="jumbotron">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+    </div>
+</head>
---
+++ index.php
@@ -0,0 +1,93 @@
+<?php include "includes/header.php" ?>
+
+<body>
+    <!-- Page Content -->
+    <section class="py-5">
+        <div class="container">
+            <h3>Reservation on the go!</h3>
+            <hr>
+            <p>Open Table : Look through our collection of restaurants and find the perfect match that best satisfy your cravings. </p>
+            <b>Making reservations simple and quick.</b>
+
+            <br>
+            <hr>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <h5>Types of Cuisines</h5>
+            <br>
+            <div class="row">
+                <div class="col-md-3 col-sm-6 mb-4">
+                    <a href="#">
+                        <img class="img-fluid-food" src="public/images/fastfood.jpg" alt="">
+                        <hr>
+                        </a>
+                    <div class="centered">Fast Food</div>
+                </div>
+                <div class="col-md-3 col-sm-6 mb-4">
+                    <a href="#">
+                    <img id="img" class="img-fluid-food" src="public/images/italian2.jpg" alt="">
+                    <hr>
+                    </a>
+                    <div class="centered">Italian Cuisine</div>
+                </div>
+                <div class="col-md-3 col-sm-6 mb-4">
+                    <a href="#">
+                    <img class="img-fluid-food" src="public/images/mexican.jpg" alt="">
+                    <hr>
+                     </a>
+                    <div class="centered">Mexican</div>
+                </div>
+                <div class="col-md-3 col-sm-6 mb-4">
+                    <a href="#">
+                    <img class="img-fluid-food" src="public/images/asian.jpg" alt="">
+                    <hr>
+                    </a>
+                    <div class="centered">Asian</div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <h5>Featured Areas</h5>
+            <br>
+            <div class="row text-center text-lg-left">
+                <div class="col-lg-3 col-md-4 col-xs-6">
+                    <a href="#">
+                        <img class="img-fluid-city" src="public/images/montreal.jpg" alt="" height="350px">
+                    </a>
+                    <hr>
+                    <div class="centered">Montreal</div>
+
+                </div>
+                <div class="col-lg-3 col-md-4 col-xs-6">
+                    <a href="#">
+                        <img class="img-fluid-city" src="public/images/toronto.jpg" alt="" height="350px">
+                    </a>
+                    <hr>
+                    <div class="centered">Toronto</div>
+
+                </div>
+                <div class="col-lg-3 col-md-4 col-xs-6">
+                    <a href="#">
+                        <img class="img-fluid-city" src="public/images/newyork2.jpg" alt="" height="350px">
+                    </a>
+                    <hr>
+                    <div class="centered">New York</div>
+
+                </div>
+                <div class="col-lg-3 col-md-4 col-xs-6">
+                    <a href="#">
+                        <img class="img-fluid-city" src="public/images/sanfran2.jpg" alt="" height="350px">
+                    </a>
+                    <hr>
+                    <div class="centered">San Francisco</div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
